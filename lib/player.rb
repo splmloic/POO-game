1ccc5c17ee61980ed12144c0e75944c99c82d549
splmloic <l2ct@protonmail.com>
2024-01-30 14:48:48 +0100

    Début de setup pour l'exo 2

--- lib/player.rb
+++ lib/player.rb
@@ -27,3 +27,46 @@ class Player
         player.gets_damage(damage)
     end
 end
+
+class HumanPlayer < Player
+    attr_accessor :name, :life_point, :weapon_level
+
+    def initialize(name)
+        @name = name.to_s
+        @life_point = 100
+        @weapon_level = 1
+    end
+
+    def show_state
+        puts "#{name} à #{life_point} points de vie, et une arme de niveau #{weapon_level}"
+    end
+
+    def compute_damage
+        rand(1..6) * @weapon_level
+    end
+
+    def search_weapon
+        weapon = rand(1..6)
+        puts "Tu as trouvé une arme de niveau #{weapon}"
+        if weapon > @weapon_level
+            @weapon_level == weapon
+            puts "Youhou ! elle est meilleure que ton arme actuelle : tu la prends."
+        else
+            puts "Merde, cette arme est une arme de daube"
+        end
+    end
+
+    def search_health_pack
+        health_pack = rand(1..6)
+        if health_pack == 1
+            puts "Tu n'a rien trouvé"
+        elsif health_pack >=2 && health_pack <=5
+            puts "Bravo, tu as trouvé un pack de +50 points de vie !"
+            @life_point = [@life_point + 50, 100].min
+        else 
+            puts "Waow, tu as trouvé un pack de +80 points de vie !"
+            @life_point = [@life_point + 80, 100].min
+        end
+    end
+    
+end
